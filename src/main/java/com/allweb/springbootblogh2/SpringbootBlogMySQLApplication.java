55efd3a154a49677debb7cc2c86129c59369e203
sangkhim <sangkhim.allweb@gmail.com>
2023-05-29 17:01:05 +0700

    format code

--- src/main/java/com/allweb/springbootblogh2/SpringbootBlogMySQLApplication.java
+++ src/main/java/com/allweb/springbootblogh2/SpringbootBlogMySQLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBlogH2Application {
+public class SpringbootBlogMySQLApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootBlogH2Application.class, args);
+        SpringApplication.run(SpringbootBlogMySQLApplication.class, args);
     }
 
 }
